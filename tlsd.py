6919173f22daa22015611f8f64ec9f1ad9e03f1d
Lucysck <125596007+Lucysck@users.noreply.github.com>
2023-09-22 17:57:59 +0800

    Update tlsd.py

--- tlsd.py
+++ tlsd.py
@@ -14,26 +14,26 @@ for arg in sys.argv[1:]:
    key,value=key_value
    params[key]=value
 
-subprocess.run(f'git clone -b master --single-branch https://github.com/Lucysck/test1.git {params["sd_dir"]}',shell=True)
+subprocess.run(f'git clone https://github.com/Lucysck/test1.git {params["sd_dir"]}',shell=True)
 
 #subprocess.run(f'git clone -b master --single-branch https://github.com/anapnoe/stable-diffusion-webui-ux {params["sd_dir"]}',shell=True)
 
-
 def task1():
  start_time = time.time()
- subprocess.run(f'cp -rf 1 /content/drive/MyDrive/sd/* {params["sd_dir"]}', shell=True)
- subprocess.run(f'git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete {params["sd_dir"]}/extensions/a1111-sd-webui-tagcomplete',shell=True)
- subprocess.run(f'git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111 {params["sd_dir"]}/extensions/ultimate-upscale',shell=True)
- subprocess.run(f'git clone https://github.com/hako-mikan/sd-webui-lora-block-weight {params["sd_dir"]}/extensions/sd-webui-lora-block-weight',shell=True)
- subprocess.run(f'git clone https://github.com/AIrjen/OneButtonPrompt {params["sd_dir"]}/extensions/OneButtonPromp',shell=True)
- subprocess.run(f'git clone https://github.com/adieyal/sd-dynamic-prompts {params["sd_dir"]}/extensions/sd-dynamic-prompts',shell=True)
+ #subprocess.run(f'cp -rf 1 /content/drive/MyDrive/sd/* {params["sd_dir"]}', shell=True)
+ #subprocess.run(f'git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete {params["sd_dir"]}/extensions/a1111-sd-webui-tagcomplete',shell=True)
+ #subprocess.run(f'git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111 {params["sd_dir"]}/extensions/ultimate-upscale',shell=True)
+ #subprocess.run(f'git clone https://github.com/hako-mikan/sd-webui-lora-block-weight {params["sd_dir"]}/extensions/sd-webui-lora-block-weight',shell=True)
+ #subprocess.run(f'git clone https://github.com/AIrjen/OneButtonPrompt {params["sd_dir"]}/extensions/OneButtonPromp',shell=True)
+ #subprocess.run(f'git clone https://github.com/adieyal/sd-dynamic-prompts {params["sd_dir"]}/extensions/sd-dynamic-prompts',shell=True)
  #subprocess.run(f'git clone https://github.com/zanllp/sd-webui-infinite-image-browsing {params["sd_dir"]}/extensions/sd-webui-infinite-image-browsing',shell=True)
- subprocess.run(f'git clone https://github.com/BlafKing/sd-civitai-browser-plus {params["sd_dir"]}/extensions/sd-civitai-browser-plus',shell=True)
+ #subprocess.run(f'git clone https://github.com/BlafKing/sd-civitai-browser-plus {params["sd_dir"]}/extensions/sd-civitai-browser-plus',shell=True)
  subprocess.run(f'git clone https://github.com/camenduru/tunnels {params["sd_dir"]}/extensions/tunnels',shell=True)
 
  if os.path.exists(f'{params["sd_dir"]}/embeddings'):
   shutil.rmtree(f'{params["sd_dir"]}/embeddings')
  subprocess.run(f'git clone https://huggingface.co/nolanaatama/embeddings {params["sd_dir"]}/embeddings',shell=True)
+
  
  end_time=time.time()
  print("\ntask1 spent:",end_time-start_time,"s")
@@ -69,6 +69,7 @@ def task4():
 def task5():
  start_time=time.time() 
  #subprocess.run("pip install xformers xformers==0.0.20",shell=True)
+ subprocess.run(f'cp -rf 1 /content/drive/MyDrive/sd/* {params["sd_dir"]}', shell=True)
  subprocess.run("pip install -q xformers==0.0.20 triton==2.0.0 -U",shell=True) 
  end_time=time.time()
  print("\ntask5 spent:",end_time-start_time,"s")
